92c5aaf9afa21201880882552968e804119701be
ilhamsj <ilhamsaputrajati@gmail.com>
2020-09-23 21:54:24 +0700

    remove useless import

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const SidebarMain = (props) => {
   return (
