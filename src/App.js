95374f91f2b17e5a5c36bb67f9d6d7006419ce22
ilhamsj <ilhamsaputrajati@gmail.com>
2020-09-23 22:23:34 +0700

    fix toggle sidebar

--- src/App.js
+++ src/App.js
@@ -9,10 +9,8 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 import Header from './Components/Header';
 import Sidebar from './Components/Sidebar';
 import Footer from './Components/Footer';
-import Banner from './Components/Banner';
 import Categories from './Components/Categories/Index';
 import Product from './Components/Product/Index';
-import SubHeader from './Components/SubHeader';
 
 const App = () => {
 
@@ -24,8 +22,6 @@ const App = () => {
           <Sidebar/>
           <div className="main-panel">
             <div className="content-wrapper">
-              <SubHeader/>
-              <Banner/>
               <Switch>
                 <Route exact path ={["/"]} component={ Categories }/>
                 <Route exact path ={["/product"]} component={ Product }/>
