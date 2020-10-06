0394ac0b7f91781fcd38e42460dbf846c7ef481b
ilhamsj <ilhamsaputrajati@gmail.com>
2020-10-06 19:49:18 +0700

    change url

--- src/Service/Connections/FruitboxConnection.js
+++ src/Service/Connections/FruitboxConnection.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: "http://fruitbox.test/",
+    baseURL: "https://foodfood.herokuapp.com/",
     headers: {
         "Content-type": "application/json"
     }
