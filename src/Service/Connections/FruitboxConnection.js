b7f0e0e4a210b9151206d75e281d8d22fc51abda
Ilham Saputrajati <ilhamsaputrajati@gmail.com>
2021-07-01 09:40:01 +0700

    update url

--- src/Service/Connections/FruitboxConnection.js
+++ src/Service/Connections/FruitboxConnection.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-    // baseURL: "https://foodfood.herokuapp.com/",
-    baseURL: "http://fruitbox.test/",
+    baseURL: "https://foodfood.herokuapp.com/",
     headers: {
         "Content-type": "application/json"
     }
